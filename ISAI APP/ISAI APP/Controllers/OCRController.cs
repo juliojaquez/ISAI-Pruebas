59c1283290637dd89a4600b80e9b1b235a5d00b9
Ulises González <ug.imt367@gmail.com>
2019-09-11 10:13:01 -0500

    Se agrega codigo que guarda en BD

--- ISAI APP/ISAI APP/Controllers/OCRController.cs
+++ ISAI APP/ISAI APP/Controllers/OCRController.cs
@@ -1,4 +1,5 @@
-﻿using ISAI_APP.Models.DTOS;
+﻿using ISAI_APP.Models;
+using ISAI_APP.Models.DTOS;
 using ISAI_APP.OCR;
 using System;
 using System.Collections.Generic;
@@ -25,7 +26,6 @@ namespace ISAI_APP.Controllers
             if (fromPrincipal != null)
             {
                 ObjOcr objOcr = FindCampos(palabras);
-                return Json(objOcr, JsonRequestBehavior.AllowGet);
             }
 
 
@@ -34,22 +34,51 @@ namespace ISAI_APP.Controllers
 
         private static ObjOcr FindCampos(List<string> palabras)
         {
-            ObjOcr ocr = new ObjOcr();
+            DataBaseJulio DataBase = new DataBaseJulio();
+            try
+            {
+                ObjOcr ocr = new ObjOcr();
 
-            var result0 = palabras.Where(x => x.Contains("NOMBRE")).ToArray();
-            var result1 = palabras.Where(x => x.Contains("CURP")).ToArray();
+                var result0 = palabras.Where(x => x.Contains("NOMBRE")).ToArray();
+                var result1 = palabras.Where(x => x.Contains("CURP")).ToArray();
 
-            int index = palabras.IndexOf(result0[0]);
-            int indexCurp = palabras.IndexOf(result1[0]);
+                int index = palabras.IndexOf(result0[0]);
+                int indexCurp = palabras.IndexOf(result1[0]);
 
-            ocr.TextoCompleto = palabras.ToArray();
-            ocr.ApellidoPat = palabras[index + 1];
-            ocr.ApellidoMat = palabras[index + 2];
-            ocr.Nombre = palabras[index + 3];
-            ocr.Curp = palabras[indexCurp + 1];
+                ocr.TextoCompleto = palabras.ToArray();
+                ocr.ApellidoPat = palabras[index + 1];
+                ocr.ApellidoMat = palabras[index + 2];
+                ocr.Nombre = palabras[index + 3];
+                ocr.Curp = palabras[indexCurp + 1];
 
+                INEData objLog = new INEData();
+                objLog.name = ocr.Nombre;
+                objLog.firstName = ocr.ApellidoPat;
+                objLog.lastName = ocr.ApellidoMat;
+                //objLog.home = "Av. de la Finca";
+                //objLog.age = 24;
+                //objLog.sex = "H";
+                //objLog.code = 80050;
+                //objLog.stateRepublic = "Puebla";
+                //objLog.stateNumber = 1;
+                //objLog.folio = 222223;
+                //objLog.registerYear = 2012;
+                objLog.CURP = ocr.Curp;
+                //objLog.stateNumber = 12;
+                //objLog.municipalityNumber = 3;
+                //objLog.locationNumber = 23;
+                //objLog.sectionNumber = 4;
+                //objLog.validUntil = 2023;
+                //objLog.codeElector = "GOJDGDGGGS2";
 
-            return ocr;
+                DataBase.INELogs.Add(objLog);
+                DataBase.SaveChanges();
+                return ocr;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
         }
     }
 }
